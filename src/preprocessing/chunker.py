2e602a7a11730a9729e5f6083658b4107c714261
Priyanka Jain <pranksb4u@gmail.com>
2026-04-18 01:24:45 +0530

    Update chunker.py

---
+++ src/preprocessing/chunker.py
@@ -0,0 +1,9 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def chunk_documents(documents, chunk_size=500, overlap=100):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=overlap
+    )
+
+    return splitter.split_documents(documents)
